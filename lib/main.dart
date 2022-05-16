834dab2697dc848ef063170d2c9f98b82e797f54
yge <xuanzong06@gmail.com>
2022-05-16 22:03:34 +0800

    Update reminder and go to store to update app

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:http/http.dart' as http;
 import 'validcode.dart';
 import 'package:intl/intl.dart';
 import 'package:marquee/marquee.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:launch_review/launch_review.dart';
 
 void main() {
   // 自訂地的Route寫在runApp裡面
@@ -20,12 +22,76 @@ void main() {
       '/DriverInfo': (context) => DriverInfo(drivername: '', carnumber: ''),
       '/PunchPage': (context) => const PunchPage(),
       '/EmployeeSalary': (context) => const EmployeeSalary(),
-      '/MaruqeeTest' : (context) => const MarquessTest(),
+      '/MaruqeeTest': (context) => const MarquessTest(),
+      '/AppInfomations': (context) => const AppInfomations(),
     },
   ));
 }
 
-//跑馬燈測試
+//自己設計的程式更新邏輯
+void CheckVersion() {
+  //先檢查程式的版本代號與資料庫的版本代號是否有不同
+}
+
+class AppInfomations extends StatefulWidget {
+  const AppInfomations({Key? key}) : super(key: key);
+
+  @override
+  State<AppInfomations> createState() => _AppInfomationsState();
+}
+
+class _AppInfomationsState extends State<AppInfomations> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  Widget _infoTile(String title, String subtitle) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle.isEmpty ? 'Not set' : subtitle),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('應用程式資訊'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _infoTile('App name', _packageInfo.appName),
+            _infoTile('Package name', _packageInfo.packageName),
+            _infoTile('App version', _packageInfo.version),
+            _infoTile('Build number', _packageInfo.buildNumber),
+            _infoTile('Build signature', _packageInfo.buildSignature),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//跑馬燈（已應用在功能選單畫面）
 class MarquessTest extends StatefulWidget {
   const MarquessTest({Key? key}) : super(key: key);
 
@@ -52,7 +118,7 @@ class _MarquessTestState extends State<MarquessTest> {
   }
 }
 
-Widget _buildSimpleMarquee(){
+Widget _buildSimpleMarquee() {
   bool _useRtlText = false;
   return Marquee(
     key: Key("$_useRtlText"),
@@ -63,13 +129,15 @@ Widget _buildSimpleMarquee(){
   );
 }
 
-Widget _wrapWithStuff(Widget child) { //在ListView中呼叫這個finction，將自定義Widget融入到ListView中
+Widget _wrapWithStuff(Widget child) {
+  //在ListView中呼叫這個finction，將自定義Widget融入到ListView中
   return Padding(
     padding: EdgeInsets.all(16.0),
     child: Container(height: 20.0, color: Colors.white, child: child),
   );
 }
 
+//員工薪資
 class EmployeeSalary extends StatefulWidget {
   const EmployeeSalary({Key? key}) : super(key: key);
 
@@ -97,6 +165,7 @@ class _EmployeeSalaryState extends State<EmployeeSalary> {
   }
 }
 
+//打卡
 class PunchPage extends StatefulWidget {
   const PunchPage({Key? key}) : super(key: key);
 
@@ -132,10 +201,12 @@ class _PunchPageState extends State<PunchPage> {
                           onPressed: () {},
                           child: Text('上班打卡'),
                         ),
-                        Padding(padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
                           child: Text('今天日期：' + nowtime),
                         ),
-                        Padding(padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
                           child: Text('上班打卡時間：'),
                         ),
                       ],
@@ -158,10 +229,12 @@ class _PunchPageState extends State<PunchPage> {
                           onPressed: () {},
                           child: Text('下班打卡'),
                         ),
-                        Padding(padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
                           child: Text('今天日期：' + nowtime),
                         ),
-                        Padding(padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(50.0, 10.0, 50.0, 0.0),
                           child: Text('下班打卡時間：'),
                         ),
                       ],
@@ -188,6 +261,7 @@ class _PunchPageState extends State<PunchPage> {
   }
 }
 
+//司機資料顯示與編輯
 class DriverInfo extends StatefulWidget {
   const DriverInfo(
       {Key? key, required this.drivername, required this.carnumber})
@@ -265,6 +339,7 @@ class _DriverInfoState extends State<DriverInfo> {
   }
 }
 
+//登入後畫面，功能選單
 class Lobby extends StatefulWidget {
   //從這邊開始都要撰寫接值的功能
   const Lobby({Key? key, required this.drivername, required this.carnumber})
@@ -293,6 +368,16 @@ class _LobbyState extends State<Lobby> {
         ),
         body: Column(
           children: [
+            Container(
+              //將跑馬燈應用在其他畫面，要研究是否必須透過Container才可以顯示
+              height: 50.0,
+              child: ListView(
+                padding: EdgeInsets.only(top: 5.0),
+                children: [
+                  _buildSimpleMarquee(),
+                ].map(_wrapWithStuff).toList(),
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -356,7 +441,7 @@ class _LobbyState extends State<Lobby> {
                 ),
                 OutlinedButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, '/EmployeeSalary');
+                    Navigator.pushNamed(context, '/AppInfomations');
                   },
                   child: Text('系統資訊'),
                 ),
@@ -386,6 +471,7 @@ class _LobbyState extends State<Lobby> {
   }
 }
 
+//登入含驗證碼司機（登入成功與失敗）
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -542,10 +628,10 @@ class _LoginPageState extends State<LoginPage> {
               context,
               MaterialPageRoute(
                   builder: (context) => Lobby(
-                    drivername: _result1,
-                    carnumber: _result2,
-                  )),
-                  (route) => false);
+                        drivername: _result1,
+                        carnumber: _result2,
+                      )),
+              (route) => false);
         } else {}
       } else {
         print('error');
@@ -556,6 +642,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
+//首頁（公司logo/與gif動畫圖放入）
 class WelcomePage extends StatefulWidget {
   //app開始顯示公司logo 或 gif動畫
   const WelcomePage({Key? key}) : super(key: key);
@@ -568,6 +655,7 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   void initState() {
     super.initState();
+    getAppVersion();
   }
 
   @override
@@ -577,13 +665,13 @@ class _WelcomePageState extends State<WelcomePage> {
 
     // 設置timer 時間到的時候自動切換頁面
     // 這個方法不能轉畫面，有錯誤；開始學習使用routes切換頁面
-    Timer(const Duration(seconds: 3), () {
-      print("3秒到");
-      // Navigator.pushNamed(context, '/login');
-
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
-    });
+    // Timer(const Duration(seconds: 3), () {
+    //   print("3秒到");
+    //   // Navigator.pushNamed(context, '/login');
+    //
+    //   Navigator.of(context)
+    //       .pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+    // });
 
     return MaterialApp(
       home: Scaffold(
@@ -599,5 +687,70 @@ class _WelcomePageState extends State<WelcomePage> {
         ),
       ),
     );
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("新版本發佈"),
+          content: new Text("請前往商店更新"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("OK"),
+              onPressed: () {
+                LaunchReview.launch(androidAppId: "com.seachaunt.seastco",
+                    // iOSAppId: "585027354"
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<String> getAppVersion() async {
+    //先取得App的資訊
+    PackageInfo _packageInfo = PackageInfo(
+      appName: 'Unknown',
+      packageName: 'Unknown',
+      version: 'Unknown',
+      buildNumber: 'Unknown',
+      buildSignature: 'Unknown',
+    );
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+
+    print(_packageInfo.appName);
+    print(_packageInfo.version);
+
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = "getAppVersion";
+      map['appname'] = _packageInfo.appName;
+      final response = await http
+          .post(Uri.parse('http://192.168.31.167:8888/atom.php'), body: map);
+      print(response.body);
+      // map[''] = "";
+      if (response.body.toString() != _packageInfo.version) {
+        //版本不相同時跳到商店 20220516
+        //版本更新訊息提示
+
+        setState(() {
+          _showDialog(context);
+        });
+        // LaunchReview.launch(androidAppId: "com.seachaunt.seastco",
+        //     // iOSAppId: "585027354"
+        // );
+      }
+
+      return "ok";
+    } catch (Exception) {
+      return "not ok";
+    }
   }
 }
